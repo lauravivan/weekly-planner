27bce4a44977e343fa8299b02d8e3de187f3990e
lauravivan <lauvivang@gmail.com>
2024-06-17 15:07:02 -0300

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Header, Cards } from "./components/index";
+import { Header, Cards, Footer } from "./components";
 
 function App() {
   return (
     <>
       <Header title="Weekly Planner" subtitle="make every day count" />
       <Cards />
+      <Footer />
     </>
   );
 }
