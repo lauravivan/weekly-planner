64049ca1f30d45224839c153be9023d3aa852f0a
lauravivan <lauvivang@gmail.com>
2024-07-10 16:55:59 -0300

    Update task type

--- src/util/localStorage.ts
+++ src/util/localStorage.ts
@@ -1,17 +1,22 @@
 import { DEFAULT_TASK_TEXT, getDayOfTheWeek } from "../util";
 
-export function getWeekTasks(): Array<string[]> {
-  const weekTasks: Array<string[]> =
+export function getWeekTasks(): Array<TaskType[]> {
+  const weekTasks: Array<TaskType[]> =
     JSON.parse(localStorage.getItem("tasks")!) ||
-    Array.from({ length: 7 }, () => [DEFAULT_TASK_TEXT]);
+    Array.from({ length: 7 }, () => [
+      {
+        taskName: DEFAULT_TASK_TEXT,
+        isChecked: false,
+      },
+    ]);
 
   return weekTasks;
 }
 
-export function getDailyTasks(dayOfTheWeek: string): Array<string> {
-  const weekTasks: Array<string[]> = getWeekTasks();
+export function getDailyTasks(dayOfTheWeek: string): Array<TaskType> {
+  const weekTasks: Array<TaskType[]> = getWeekTasks();
   const weekIndex: number = getDayOfTheWeek(dayOfTheWeek);
-  const dailyTasks: Array<string> = weekTasks[weekIndex];
+  const dailyTasks: Array<TaskType> = weekTasks[weekIndex];
 
   return dailyTasks;
 }
